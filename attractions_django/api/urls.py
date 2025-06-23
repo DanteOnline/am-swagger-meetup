933a670bbbff645006420e329bac63cb5d7d4045
DanteOnline <iamdanteonline@gmail.com>
2025-06-23 20:34:30 +0300

    add swagger generation

--- attractions_django/api/urls.py
+++ attractions_django/api/urls.py
@@ -2,6 +2,7 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from .views import AttractionViewSet
 
+
 app_name = 'api'  # pylint:disable=invalid-name
 
 router = DefaultRouter()
--- attractions_django/attractions_django/urls.py
+++ attractions_django/attractions_django/urls.py
@@ -5,15 +5,18 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 
-schema_view = get_schema_view(  # pylint:disable=invalid-name
-   openapi.Info(
+openapi_info = openapi.Info(
       title="Attractions API",
       default_version='v1',
       description="For AM Meetup",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="iamdanteonline@gmail.com"),
       license=openapi.License(name="BSD License"),
-   ),
+   )
+
+
+schema_view = get_schema_view(  # pylint:disable=invalid-name
+   # openapi_info,
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
